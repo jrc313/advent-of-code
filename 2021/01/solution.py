d3e258509b16580363daf037252e2a76bffd328e
James Corbett <james@jrc313.com>
2021-12-01 17:38:29 +0000

    Optimisations based on Paul's observation that you don't need to sum for part 2

--- 2021/01/solution.py
+++ 2021/01/solution.py
@@ -2,47 +2,45 @@ import os
 import argparse
 from itertools import starmap
 
-# Return true if b is deeper than a
-def isDeeper(a, b):
-    return int(b) > int(a)
-
 # Get pairs of items from iterable
-def pairwise(iterable):
-    for i in range(1, len(iterable)):
-        yield [iterable[i - 1], iterable[i]]
+def pairwise(iterable, distance = 1):
+    for i in range(distance, len(iterable)):
+        yield [iterable[i - distance], iterable[i]]
 
-# Calculate rolling 3 sum
-def triple_sum(iterable):
-    for i in range(2, len(iterable)):
-        yield int(iterable[i - 2]) + int(iterable[i - 1]) + int(iterable[i])
+def solve(input, distance):
+    return sum((starmap(lambda a, b: a < b, pairwise(input, distance))))
 
 # Part 1
 def solve1(input):
-    answer = sum((starmap(isDeeper, pairwise(input))))
-    print(f"{answer}")
+    return solve(input, 1)
 
 # Part 2
 def solve2(input):
-    sums = list(triple_sum(input))
-    answer = answer = sum((starmap(isDeeper, pairwise(sums))))
-    print(f"{answer}")
+    return solve(input, 3)
 
 # Testing, testing
 def test():
     # TODO
-    print(f"")
+    return "TODO"
 
+def get_input(filename = "input.txt", line_parser = lambda line: int(line)):
+    input_path = os.path.dirname(os.path.realpath(__file__)) + f"/{filename}"
+    input = []
+    with open(input_path, "r") as file:
+        for line in file:
+            input.append(line_parser(line.rstrip()))
 
-input_path = os.path.dirname(os.path.realpath(__file__)) + "/input.txt"
-input = []
-with open(input_path, "r") as file:
-    input = file.read().splitlines()
+    return input
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--test", action = "store_true")
 args = parser.parse_args()
+
+input = get_input()
+
 if args.test:
-    test()
+    print(f"Test: {test()}")
 else:
-    solve1(input)
-    solve2(input)
+    print(f"Part 1: {solve1(input)}")
+    print(f"Part 2: {solve2(input)}")
--- day_template.py
+++ day_template.py
@@ -5,29 +5,37 @@ import argparse
 # Part 1
 def solve1(input):
     # TODO
-    print(f"")
+    return "TODO"
 
 # Part 2
 def solve2(input):
     # TODO
-    print(f"")
+    return "TODO"
 
 # Testing, testing
 def test():
     # TODO
-    print(f"")
+    return "TODO"
 
 
-input_path = os.path.dirname(os.path.realpath(__file__)) + "/input.txt"
-input = []
-with open(input_path, "r") as file:
-    input = file.read().splitlines()
+def get_input(filename = "input.txt", line_parser = lambda line: int(line)):
+    input_path = os.path.dirname(os.path.realpath(__file__)) + f"/{filename}"
+    input = []
+    with open(input_path, "r") as file:
+        for line in file:
+            input.append(line_parser(line.rstrip()))
+
+    return input
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--test", action = "store_true")
 args = parser.parse_args()
+
+input = get_input()
+
 if args.test:
-    test()
+    print(f"Test: {test()}")
 else:
-    solve1(input)
-    solve2(input)
+    print(f"Part 1: {solve1(input)}")
+    print(f"Part 2: {solve2(input)}")
