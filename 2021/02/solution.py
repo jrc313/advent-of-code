e148b9123a108af0c6a81971e3dc286958a0a1d3
James Corbett <james@jrc313.com>
2021-12-02 20:54:20 +0000

    Day 2 optimisation

--- 2021/02/solution.py
+++ 2021/02/solution.py
@@ -1,40 +1,33 @@
+import functools
 import os
 import argparse
-
-
+ 
+H = 0
+D = 1
+A = 2
+ 
 # Part 1
 def solve1(input):
-    h = 0
-    d = 0
-    for command, arg in input:
-        if command == "forward":
-            h += int(arg)
-        if command == "down":
-            d += int(arg)
-        if command == "up":
-            d -= int(arg)
-    return f"h: {h}, d: {d}, mul: {h * d}"
-
+    proc = {"forward": lambda ub, arg: [ub[H] + arg, ub[D]],
+            "down": lambda ub, arg: [ub[H], ub[D] + arg],
+            "up": lambda ub, arg: [ub[H], ub[D] - arg]}
+   
+    ub = functools.reduce(lambda ub, item: proc[item[0]](ub, int(item[1])), input, [0, 0, 0])
+    return f"h: {ub[H]}, d: {ub[D]}, mul: {ub[H] * ub[D]}"
+ 
 # Part 2
 def solve2(input):
-    h = 0
-    a = 0
-    d = 0
-    for command, arg in input:
-        if command == "forward":
-            h += int(arg)
-            d += a * int(arg)
-        if command == "down":
-            a += int(arg)
-        if command == "up":
-            a -= int(arg)
-    return f"h: {h}, d: {d}, mul: {h * d}"
-
+    proc = {"forward": lambda ub, arg: [ub[H] + arg, ub[D] + (ub[A] * arg), ub[A]],
+            "down": lambda ub, arg: [ub[H], ub[D], ub[A] + arg],
+            "up": lambda ub, arg: [ub[H], ub[D], ub[A] - arg]}
+   
+    ub = functools.reduce(lambda ub, item: proc[item[0]](ub, int(item[1])), input, [0, 0, 0])
+    return f"h: {ub[H]}, d: {ub[D]}, mul: {ub[H] * ub[D]}"
+ 
 # Testing, testing
 def test():
-    # TODO
     return "TODO"
-
+ 
 
 def get_input(filename = "input.txt", line_parser = lambda line: int(line)):
     input_path = os.path.dirname(os.path.realpath(__file__)) + f"/{filename}"
@@ -42,16 +35,16 @@ def get_input(filename = "input.txt", line_parser = lambda line: int(line)):
     with open(input_path, "r") as file:
         for line in file:
             input.append(line_parser(line.rstrip()))
-
+ 
     return input
-
+ 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--test", action = "store_true")
 args = parser.parse_args()
-
+ 
 input = get_input(line_parser = lambda line: line.split())
-
+ 
 if args.test:
     print(f"Test: {test()}")
 else:
