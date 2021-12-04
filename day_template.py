e53b38271f1b16773688139641107e51a38e3fb1
James Corbett <james@jrc313.com>
2021-12-04 14:33:52 +0000

    Simplify get_input method in template

--- day_template.py
+++ day_template.py
@@ -18,14 +18,9 @@ def test():
     return "TODO"
 
 
-def get_input(filename = "input.txt", line_parser = lambda line: int(line)):
+def get_input(filename = "input.txt"):
     input_path = os.path.dirname(os.path.realpath(__file__)) + f"/{filename}"
-    input = []
-    with open(input_path, "r") as file:
-        for line in file:
-            input.append(line_parser(line.rstrip()))
-
-    return input
+    return [line.rstrip() for line in open(input_path)]
 
 
 parser = argparse.ArgumentParser()
