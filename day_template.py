eeff364fc4ad5c85b18556aa7a898b3466fa7e91
James Corbett <james@jrc313.com>
2021-12-05 00:38:41 +0000

    Simplify template

--- day_template.py
+++ day_template.py
@@ -18,19 +18,20 @@ def test():
     return "TODO"
 
 
-def get_input(filename = "input.txt"):
+
+def get_input(filename):
     input_path = os.path.dirname(os.path.realpath(__file__)) + f"/{filename}"
     return [line.rstrip() for line in open(input_path)]
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--test", action = "store_true")
 args = parser.parse_args()
 
-input = get_input()
-
+filename = "input.txt"
 if args.test:
-    print(f"Test: {test()}")
-else:
-    print(f"Part 1: {solve1(input)}")
-    print(f"Part 2: {solve2(input)}")
+    filename = "test.txt"
+
+input = get_input(filename)
+
+print(f"Part 1: {solve1(input)}")
+print(f"Part 2: {solve2(input)}")
