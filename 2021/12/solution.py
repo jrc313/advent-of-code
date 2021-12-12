15fb7f3a078bd8cf88c7f048a618fb4366a79228
James Corbett <james@jrc313.com>
2021-12-12 15:20:45 +0000

    2021 Day 12

---
+++ 2021/12/solution.py
@@ -0,0 +1,93 @@
+import os
+import argparse
+from collections import defaultdict, deque
+from functools import reduce
+
+class Graph:
+    def __init__(self):
+        self.path_count = 0
+        self.paths = []
+        self.little_caves = set()
+        self.graph = defaultdict(list)
+
+    def addEdge(self, a, b):
+        self.__addEdge(a, b)
+        self.__addEdge(b, a)
+
+    def __addEdge(self, a, b):
+        if (a == "end"): return
+        if (b == "start"): return
+        self.graph[a].append(b)
+        if a.lower() == a and a not in ["start", "end"]: self.little_caves.add(a)
+
+    def doSearch(self, a, target, visited = set(), current_path = deque(), allow_two = ""):
+        
+        current_path.append(a)
+        visit_count = count_instances(current_path, a)
+        if a.lower() == a and (not(a == allow_two) or visit_count >= 2):
+            visited.add(a)
+
+        if a == target:
+            self.paths.append(",".join(current_path))
+            self.path_count += 1
+
+        else:
+            for b in self.graph[a]:
+                if b not in visited:
+                    self.doSearch(b, target, visited, current_path, allow_two)
+
+        current_path.remove(a)
+        visited.discard(a)
+
+    def search(self, start, target, allow_two):
+        self.paths.clear()
+        self.path_count = 0
+        self.doSearch(start, target, allow_two = allow_two)
+        return self.path_count
+
+def count_instances(q, a):
+    return len(list(filter(lambda n: a == n, q)))
+
+# Part 1
+def solve1(input):
+    g = Graph()
+    for line in input:
+        edge = line.split("-")
+        g.addEdge(edge[0], edge[1])
+
+    total = g.search("start", "end", "")
+    return total
+
+# Part 2
+def solve2(input):
+    g = Graph()
+    for line in input:
+        edge = line.split("-")
+        g.addEdge(edge[0], edge[1])
+
+    original_total = g.search("start", "end", "")
+    total = original_total
+    for a in g.little_caves:
+        count = g.search("start", "end", a)
+        total += count - original_total
+
+    return total
+
+
+
+def get_input(filename):
+    input_path = os.path.dirname(os.path.realpath(__file__)) + f"/{filename}"
+    return [line.rstrip() for line in open(input_path)]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--test", action = "store_true")
+args = parser.parse_args()
+
+filename = "input.txt"
+if args.test:
+    filename = "test.txt"
+
+input = get_input(filename)
+
+print(f"Part 1: {solve1(input)}")
+print(f"Part 2: {solve2(input)}")
--- start-day.sh
+++ start-day.sh
@@ -7,7 +7,8 @@ DAY_0=$(printf "%02d" $2)
 BASEDIR="./${YEAR}/${DAY_0}"
 SOLUTION_FILE="${BASEDIR}/solution.py"
 SESSION=$(<.env)
-INPUT_URL="https://adventofcode.com/${YEAR}/day/${DAY}/input"
+PUZZLE_URL="https://adventofcode.com/${YEAR}/day/${DAY}"
+INPUT_URL="${PUZZLE_URL}/input"
 INPUT_FILE="${BASEDIR}/input.txt"
 TEST_FILE="${BASEDIR}/test.txt"
 
@@ -15,3 +16,4 @@ TEST_FILE="${BASEDIR}/test.txt"
 [ ! -f "${SOLUTION_FILE}" ] && cp "${DAY_TEMPLATE}" "${SOLUTION_FILE}"
 [ ! -f "${TEST_FILE}" ] && touch "${TEST_FILE}"
 [ ! -f "${INPUT_FILE}" ] && curl -H "Cookie: ${SESSION}" "${INPUT_URL}" --output "${INPUT_FILE}"
+Open "${PUZZLE_URL}"
