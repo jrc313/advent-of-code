8db8da9f4d0ff816a92da345769cff4242d4c869
James Corbett <james@jrc313.com>
2022-12-03 13:33:48 +0000

    Switch to set-intersect

--- racket/2022/03/solution.rkt
+++ racket/2022/03/solution.rkt
@@ -18,38 +18,30 @@
 (define (rucksacks->compartments rucksacks)
     (map (λ (rucksack) (cons
         (substring rucksack 0 (/ (string-length rucksack) 2))
-        (substring rucksack (/ (string-length rucksack) 2)))) rucksacks))
-
-(define (find-dupes rucksacks)
-    (map (λ (rucksack)
-        (foldl (λ (item current)
-            (if (string-contains? (cdr rucksack) (~a item)) item current))
-            "" (string->list (car rucksack)))) rucksacks))
-
-(define (get-priorities items)
-    (map (λ (item) (- (char->integer item) (if (char-upper-case? item) 38 96))) items))
+        (cons (substring rucksack (/ (string-length rucksack) 2)) '()))) rucksacks))
 
 (define (group-three l)
     (if (empty? l) '()
         (cons (cons (car l) (cons (cadr l) (cons (caddr l) '()))) (group-three (cdddr l)))))
 
-(define (find-dupes-three threes)
-    (map (λ (group)
-        (foldl (λ (item current)
-            (if (and (string-contains? (cadr group) (~a item))
-                     (string-contains? (caddr group) (~a item)))
-                item current))
-            "" (string->list (car group)))) threes))
-
 (define (sum-priorities items)
     (foldl (λ (item total)
-        (+ total (- (char->integer item) (if (char-upper-case? item) 38 96)))) 0 items))
+        (+ total (- (char->integer item) (if (char-upper-case? item) 38 96))))
+        0 items))
 
 (define (part1 aoc-input)
-    (sum-priorities (find-dupes (rucksacks->compartments aoc-input))))
+    (sum-priorities (map (λ (rucksack)
+        (car (set-intersect
+            (string->list (car rucksack))
+            (string->list (cadr rucksack)))))
+        (rucksacks->compartments aoc-input))))
 
 (define (part2 aoc-input)
-    (sum-priorities(find-dupes-three (group-three aoc-input))))
+    (sum-priorities(map (λ (group)
+        (car (set-intersect
+            (string->list (car group))
+            (string->list (cadr group))
+            (string->list (caddr group))))) (group-three aoc-input))))
 
 (let ([aoc-input (parse-aoc-input (if (test-mode) "test.txt" "input.txt"))])
     (time (printf "Part1: ~a\n" (part1 aoc-input)))
