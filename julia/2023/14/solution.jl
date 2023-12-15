a4b4a9d4dfce8c8667502c76ea2833995e303fe4
James Corbett <james@jrc313.com>
2023-12-15 01:28:50 +0000

    Minor perf enhancements for 2023 day 14

--- julia/2023/14/solution.jl
+++ julia/2023/14/solution.jl
@@ -20,14 +20,17 @@ module Aoc202314
 
     function solve(test::Bool)
 
-        platform = parseinput(test)
+        platform::Matrix{Int} = parseinput(test)
+        w::Int = size(platform)[1]
+        fwdrng::UnitRange{Int} = 1:w
+        bckrng::StepRange{Int} = w:-1:1
 
         megaload::Int = 0
         spinloads::Vector{Vector{Int}} = []
         found::Bool = false
         i::Int = 1
         while !found
-            nextload::Vector{Int} = fastspincycle(platform)
+            nextload::Vector{Int} = fastspincycle(platform, w, fwdrng, bckrng)
             cycleindex::Union{Nothing, Int} = findfirst(l -> l == nextload, spinloads)
             if !isnothing(cycleindex)
                 cyclelength = i - cycleindex
@@ -38,23 +41,22 @@ module Aoc202314
             i += 1
         end
 
-        part1 = spinloads[1][1]
-        part2 = spinloads[megaload][2]
+        part1::Int = spinloads[1][1]
+        part2::Int = spinloads[megaload][2]
 
         return (part1, part2)
     end
 
-    function fastspincycle(platform::Matrix)
-        w = size(platform)[1]
-        north::Int = tiltplatformfast(w, 1:w, 1:w, (a, b) -> platform[b, a], (a, b, v) -> platform[b, a] = v, <, +)
-        tiltplatformfast(w, 1:w, 1:w, (a, b) -> platform[a, b], (a, b, v) -> platform[a, b] = v, <, +)
-        south::Int = tiltplatformfast(w, 1:w, w:-1:1, (a, b) -> platform[b, a], (a, b, v) -> platform[b, a] = v, >, -)
-        tiltplatformfast(w, 1:w, w:-1:1, (a, b) -> platform[a, b], (a, b, v) -> platform[a, b] = v, >, -)
+    function fastspincycle(platform::Matrix, w::Int, fwdrng::UnitRange{Int}, bckrng::StepRange{Int})
+        north::Int = tiltplatformfast(w, fwdrng, fwdrng, (a, b) -> platform[b, a], (a, b, v) -> platform[b, a] = v, <, +)
+        tiltplatformfast(w, fwdrng, fwdrng, (a, b) -> platform[a, b], (a, b, v) -> platform[a, b] = v, <, +)
+        south::Int = tiltplatformfast(w, fwdrng, bckrng, (a, b) -> platform[b, a], (a, b, v) -> platform[b, a] = v, >, -)
+        tiltplatformfast(w, fwdrng, bckrng, (a, b) -> platform[a, b], (a, b, v) -> platform[a, b] = v, >, -)
         return [north, south]
     end
 
     function tiltplatformfast(h::Int,
-                              outerrng::Union{UnitRange{Int}, StepRange{Int}}, innerrng::Union{UnitRange{Int}, StepRange{Int}},
+                              outerrng::UnitRange{Int}, innerrng::Union{UnitRange{Int}, StepRange{Int}},
                               getfn::Function, setfn::Function, comparefn::Function, incfn::Function)
         loadsum::Int = 0
         for m::Int in outerrng
@@ -67,7 +69,6 @@ module Aoc202314
                     setfn(m, n, 0)
                     setfn(m, lastoccupied, 2)
                 else
-                    v == 2 && (loadsum += h - lastoccupied + 1)
                     v != 0 && (lastoccupied = n)
                 end
             end
