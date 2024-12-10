63d8c5ff836b2689c3e0b52dcc6bbd52473772fd
James Corbett <james@jrc313.com>
2024-12-10 17:26:15 +0000

    Day 8 of 2024

--- julia/2024/08/solution.jl
+++ julia/2024/08/solution.jl
@@ -16,18 +16,52 @@ module Aoc202408
         return solve(false)
     end
 
+    @inline function isinbounds(bounds::Tuple{Int, Int}, a::CartesianIndex)
+        return 0 < a[1] <= bounds[1] && 0 < a[2] <= bounds[2]
+    end
+
     function solve(test::Bool)
 
-        input = parseinput(test)
+        grid = parseinput(test)
+        bounds = size(grid)
+        nodes = Dict{Char, Vector{CartesianIndex}}()
+        for (pos, type) in pairs(IndexCartesian(), grid)
+            type == '.' && continue
+            node = get!(() -> Int[], nodes, type)
+            push!(node, pos)
+        end
 
-        part1 = 0
-        part2 = 0
+        antinodes = Set{CartesianIndex}()
+        manyantinodes = Set{CartesianIndex}()
+        for positions in values(nodes)
+            for (a, b) in Iterators.product(positions, positions)
+                a == b && continue
+                dist = a - b
+                an1 = a + dist
+                an2 = b - dist
+                isinbounds(bounds, an1) && push!(antinodes, an1)
+                isinbounds(bounds, an2) && push!(antinodes, an2)
+                push!(manyantinodes, a)
+                push!(manyantinodes, b)
+                while isinbounds(bounds, an1)
+                    push!(manyantinodes, an1)
+                    an1 = an1 + dist
+                end
+                while isinbounds(bounds, an2)
+                    push!(manyantinodes, an2)
+                    an2 = an2 - dist
+                end
+            end
+        end
+
+        part1 = length(antinodes)
+        part2 = length(manyantinodes)
 
         return (part1, part2)
     end
 
     function parseinput(test::Bool)
-        return AocUtils.getinput(YEAR, DAY, test)
+        return AocUtils.loadcharmatrix(YEAR, DAY, test)
     end
 
 end
