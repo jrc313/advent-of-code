ddc90454222774d5857f15e6725b63673a73fb36
James Corbett <james@jrc313.com>
2025-12-14 17:09:47 +0000

    2025 day 8

--- julia/2025/08/solution.jl
+++ julia/2025/08/solution.jl
@@ -32,29 +32,32 @@ module Aoc202508
         boxes::Vector{Point3d} = [parse(Point3d, line) for line in AocUtils.eachinputlines(YEAR, DAY, test)]
         boxdists::Vector{BoxPair} = []
         
-        for (i, box1) in enumerate(boxes)
-            for box2 in boxes[i+1:end]
-                push!(boxdists, BoxPair(box1, box2, distance(box1, box2)))
+        for (i::Int, box1::Point3d) in enumerate(boxes)
+            for box2::Point3d in boxes[i+1:end]
+                pair::BoxPair = BoxPair(box1, box2, distance(box1, box2))
+                push!(boxdists, pair)
             end
         end
 
-        sort!(boxdists, by = a -> a.dist)
+        sort!(boxdists, by = a -> a.dist, alg = QuickSort)
 
         circuits::Vector{Set{Point3d}} = []
 
-        connected = Set()
+        connected::Set{Point3d} = Set()
 
-        connections = 0
-        pairnum = 1
+        connections::Int = 0
+        pairnum::Int = 1
+        boxcount::Int = length(boxes)
+        connectedcount::Int = 0
 
-        while length(connected) < length(boxes)
-            pair = boxdists[pairnum]
+        while connectedcount < boxcount
+            pair::BoxPair = boxdists[pairnum]
             pairnum += 1
             connections += 1
-            boxacircuit = 0
-            boxbcircuit = 0
+            boxacircuit::Int = 0
+            boxbcircuit::Int = 0
            
-            for (i, circuit) in enumerate(circuits)
+            for (i::Int, circuit::Set{Point3d}) in enumerate(circuits)
                 boxacircuit == 0 && in(pair.a, circuit) && (boxacircuit = i)
                 boxbcircuit == 0 && in(pair.b, circuit) && (boxbcircuit = i)
             end
@@ -73,15 +76,13 @@ module Aoc202508
             union!(connected, [pair.a, pair.b])
 
             if connections == (test ? 10 : 1000)
-                circuitlengths = length.(circuits)
-                sort!(circuitlengths, rev = true)
+                circuitlengths::Vector{Int} = length.(circuits)
+                partialsort!(circuitlengths, 3, rev = true)
                 part1 = reduce(*, circuitlengths[1:3])
             end
-            length(connected) == length(boxes) && (part2 = pair.a.x * pair.b.x)
+            connectedcount = length(connected)
+            connectedcount == boxcount && (part2 = pair.a.x * pair.b.x)
         end
-
-        connected = reduce((acc, circuit) -> union(acc, circuit), circuits, init = Set())
-        
 
         return (part1, part2)
     end
